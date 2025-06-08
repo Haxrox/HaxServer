94a92eacf1e3b240ceb1b4b79f6da4c855f8f212
Haxrox <56497846+Haxrox@users.noreply.github.com>
2025-06-08 13:52:50 -0700

    Fixes #6: added certificates + DNS

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ const path = require('path');
 
 const app = express();
 
-const HTTP_PORT = process.env.HTTP_PORT || 8080;
-const HTTPS_PORT = process.env.HTTPS_PORT || 4430;
-
+require('dotenv').config();
+const HTTP_PORT = process.env.HTTP_PORT || 80;
+const HTTPS_PORT = process.env.HTTPS_PORT || 443;
+const HTTPS_KEY = process.env.HTTPS_KEY || path.join(__dirname, 'selfsigned.key');
+const HTTPS_CERT = process.env.HTTPS_CERT || path.join(__dirname, 'selfsigned.cert');
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/out/index.html'));
@@ -34,14 +36,10 @@ app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'frontend/out/404.html'));
 });
 
-http.createServer(app).listen(HTTP_PORT, () => {
-  console.log(`HTTP Server is running on port ${HTTP_PORT}`);
-});
-
 // Create an HTTPS server
 const options = {
-  key: fs.readFileSync(path.join(__dirname, 'selfsigned.key')),
-  cert: fs.readFileSync(path.join(__dirname, 'selfsigned.crt'))
+  key: fs.readFileSync(HTTPS_KEY),
+  cert: fs.readFileSync(HTTPS_CERT)
 };
 
 const httpsServer = https.createServer(options, app);
@@ -49,3 +47,11 @@ httpsServer.listen(HTTPS_PORT, () => {
   console.log(`HTTPS Server is running on port ${HTTPS_PORT}`);
 });
 
+http.createServer((req, res) => {
+  // Redirect HTTP requests to HTTPS
+  res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+  res.end();
+}).listen(HTTP_PORT, () => {
+  console.log(`HTTP Server is running on port ${HTTP_PORT}`);
+  console.log(`Redirecting HTTP to HTTPS on port ${HTTPS_PORT}`);
+});
