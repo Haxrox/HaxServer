28def640f92d6505cc434a086dbeba7d1ae6ee24
Haxrox <56497846+Haxrox@users.noreply.github.com>
2025-07-05 17:18:22 -0700

    Fixes #13: add duration tracking on main page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import type { Metadata } from "next";
 import "./globals.css";
 import FluentUIProvider from "../components/FluentProvider";
+import { useEffect } from "react";
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: "HaxServer",
   description: "Pointer to all web applications created by HaxTech",
   icons: {
@@ -12,11 +15,48 @@ export const metadata: Metadata = {
   },
 };
 
+function send(
+  endpoint: string,
+  method = "GET",
+  data: {
+    tag: string;
+    log: string;
+  }
+) {
+  console.log(`${endpoint} | data: `, data);
+  return fetch(endpoint, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Handle closing tab
+  useEffect(() => {
+    const startTime = new Date();
+    console.log('HaxTech started at ' + startTime);
+    window.addEventListener('beforeunload', async function (evt) {
+      const endTime = new Date();
+      const duration = (endTime.valueOf() - startTime.valueOf() / 1000);
+
+      await send('api/log', "POST", {
+        tag: "HaxTech",
+        log: `Duration: ${duration}s`
+      }).then((response) => {
+        console.log('Close request sent | response: ', response);
+      }).catch(error => {
+        console.error('Error sending close request:', error);
+      });
+    });
+  }, [])
+
   return (
     <html lang="en">
       <body>
